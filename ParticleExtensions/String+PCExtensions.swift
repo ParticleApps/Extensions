6842c46dc8f4aca28706b9a732c9134ec973f0fd
Rocco Del Priore <roccodelpriore@me.com>
2018-10-02 23:12:52 -0700

    Add string convience methods.

--- ParticleExtensions/String+PCExtensions.swift
+++ ParticleExtensions/String+PCExtensions.swift
@@ -29,4 +29,48 @@ extension String {
         }
         return mutableString as String
     }
+    public func onlyNumericalValues() -> String {
+        return components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+    }
+    public func onlyAlphabeticalValues() -> String {
+        return components(separatedBy: CharacterSet.letters.inverted).joined()
+    }
+    public func withoutNumericalValues() -> String {
+        return components(separatedBy: CharacterSet.decimalDigits).joined()
+    }
+    public func withoutAlphabeticalValues() -> String {
+        return components(separatedBy: CharacterSet.letters).joined()
+    }
+    public func asPhoneNumber(spaces: Bool = false) -> String {
+        let number = self.onlyNumericalValues()
+        if number.count == 7 {
+            let firstThreeDigits = number.prefix(3)
+            let lastFourDigits = number.dropFirst(3)
+            if spaces {
+                return String.init(format: "%@ - %@", firstThreeDigits as CVarArg, lastFourDigits as CVarArg)
+            }
+            return String.init(format: "%@-%@", firstThreeDigits as CVarArg, lastFourDigits as CVarArg)
+        }
+        else if number.count == 10 {
+            let firstThreeDigits = number.prefix(3)
+            let middleThreeDigits = number[number.index(number.startIndex, offsetBy: 3)..<number.index(number.endIndex, offsetBy: -4)]
+            let lastFourDigits = number.dropFirst(6)
+            if spaces {
+                return String.init(format: "(%@) %@ - %@", firstThreeDigits as CVarArg, middleThreeDigits as CVarArg, lastFourDigits as CVarArg)
+            }
+            return String.init(format: "(%@)%@-%@", firstThreeDigits as CVarArg, middleThreeDigits as CVarArg, lastFourDigits as CVarArg)
+        }
+        else if number.count > 10 {
+            let prefixCount = number.count-10
+            let firstNDigits = number.prefix(prefixCount)
+            let firstThreeDigits = number[number.index(number.startIndex, offsetBy: prefixCount)..<number.index(number.endIndex, offsetBy: -7)]
+            let middleThreeDigits = number[number.index(number.startIndex, offsetBy: prefixCount+3)..<number.index(number.endIndex, offsetBy: -4)]
+            let lastFourDigits = number.dropFirst(number.count-4)
+            if spaces {
+                return String.init(format: "+%@ (%@) %@ - %@", firstNDigits as CVarArg, firstThreeDigits as CVarArg, middleThreeDigits as CVarArg, lastFourDigits as CVarArg)
+            }
+            return String.init(format: "+%@(%@)%@-%@", firstNDigits as CVarArg, firstThreeDigits as CVarArg, middleThreeDigits as CVarArg, lastFourDigits as CVarArg)
+        }
+        return number
+    }
 }
